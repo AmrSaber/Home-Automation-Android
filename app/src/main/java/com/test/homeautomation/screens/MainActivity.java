2de5c6edb47d4ac7a889872c359800951686c937
ManarArabi <manar.araby.ma@gmail.com>
2019-04-20 17:41:32 +0200

    popup is fixed, dark mode is enabled

--- app/src/main/java/com/test/homeautomation/screens/MainActivity.java
+++ app/src/main/java/com/test/homeautomation/screens/MainActivity.java
@@ -7,50 +7,29 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.PopupWindow;
 import com.test.homeautomation.R;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
+    PopupWindow add_device_popup ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        add_device_popup =  new PopupWindow();
         setContentView(R.layout.activity_main);
     }
 
+
     public void add_device_popup(View view){
-        //popup init
-        // inflate the layout of the popup window
-        LayoutInflater inflater = (LayoutInflater)
-                getSystemService(LAYOUT_INFLATER_SERVICE);
-        View popupView = inflater.inflate(R.layout.add_device_popup, null);
-
-        // create the popup window
-        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
-        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-        boolean focusable = true; // lets taps outside the popup also dismiss it
-        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
-
-
-        // show the popup window
-        // which view you pass in doesn't matter, it is only used for the window tolken
-        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
-
-
+        add_device_popup.init_popup(this);
+        add_device_popup.show_popup(0, 0);
     }
 
     public void submit_device(View view){
         //submit device
-        dismiss_popup();
+        add_device_popup.dismiss_popup();
 
     }
-    public void dismiss_popup(){
-        //popupWindow.dismiss();
-    }
 
-    public void back_button(View view){
-        dismiss_popup();
-    }
+
 }
