c4d4f1f91342da3517a24e33a8f6f7086e80b12c
ManarArabi <manar.araby.ma@gmail.com>
2019-04-20 21:54:34 +0200

    updates

--- app/src/main/java/com/test/homeautomation/screens/SplashActivity.java
+++ app/src/main/java/com/test/homeautomation/screens/SplashActivity.java
@@ -10,16 +10,25 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Timer timer = new Timer();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_activity);
 
-        new Timer().schedule(new TimerTask() {
+        timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
         }, 2000);
+        this.finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        timer.cancel();
     }
 }
