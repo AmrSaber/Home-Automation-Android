a88a6358423b8f805fa1e84f2a4246bd7da845ad
ManarArabi <manar.araby.ma@gmail.com>
2019-04-20 15:28:25 +0200

    initial commit

---
+++ app/src/main/java/com/test/homeautomation/screens/MainActivity.java
@@ -0,0 +1,56 @@
+package com.test.homeautomation.screens;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import com.test.homeautomation.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void add_device_popup(View view){
+        //popup init
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.add_device_popup, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+
+    }
+
+    public void submit_device(View view){
+        //submit device
+        dismiss_popup();
+
+    }
+    public void dismiss_popup(){
+        //popupWindow.dismiss();
+    }
+
+    public void back_button(View view){
+        dismiss_popup();
+    }
+}
